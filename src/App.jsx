bbbe0a017318df80e0306facba90cf5a21552514
Farab-Hussain <sudyanbaloch302@gmail.com>
2024-10-14 00:43:49 +0500

    SetUp

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./sections/Header";
+
+const App = () => {
+  return (
+    <main>
+      <Header /> {/* Render the Header component */}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </main>
+  );
+};
+
+export default App;
